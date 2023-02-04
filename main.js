6c6600690dbcc09d2cb37c7141062550a6353927
Guilherme Fidelis <guilhermehfidelis@gmail.com>
2023-02-04 17:41:07 -0300

    Add the initial methods for fetching the playlist website and get the last song played every 30 seconds.

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 // Require HTTP header
 var http = require('http');
+
+// Read the Online Radio Box playlist
+const url = 'https://onlineradiobox.com/uk/absolute1058/playlist/';
+const selector = '#bodyW > div.page > div > div.page__wrapper-4right > div > section > table > tbody > tr.active > td.track_history_item > a';
+var lastSong;
+
+// Function to get the last song played with the url and the selector provided
+async function getLastSong(url, selector) {
+  var result;
+  // Use Axios to fetch the website
+  await axios.get(url).then((response) => {
+    const body = response.data;
+    // Load the response into cheerio
+    const $ = cheerio.load(body);
+    // Access the element that has the last song played
+    if (lastSong != $(selector).html()) {
+      // Set the last song if the current song is different
+      lastSong = $(selector).html();
+      console.log(lastSong);
+    }
+  });
+  return lastSong;
+}
+
+// Initial get
+getLastSong(url, selector);
+
+// Keep looking the playlist every 30 seconds
+async function update() {
+  await getLastSong(url, selector);
+}
+setInterval(update, 30000);
 
 // Create HTTP server
 http.createServer(function (req, res) {
@@ -8,7 +43,7 @@ http.createServer(function (req, res) {
   // Content Type: text/html
   res.writeHead(200, {'Content-Type': 'text/html'});
 
-  // Send response body as "Hello World"
-  res.end('Hello World');
+  // Send the last song played
+  res.end(lastSong);
 
 }).listen(8080);
